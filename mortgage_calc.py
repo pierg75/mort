8669df436d0fedb85c37f89f3cc4a7b8f1978fb6
Pierguido Lambri <plambri@redhat.com>
2022-08-25 19:02:45 +0100

    Added body of the program
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- mortgage_calc.py
+++ mortgage_calc.py
@@ -1,4 +1,27 @@
 
+class Mortgage():
+    
+    def __init__(self, sum, years, irate):
+        self.sum = sum
+        self.years = years
+        self.irate = irate
+
+    def calculate(self):
+        monthly = self.sum*(self.irate/100/12)*(((1 + (self.irate/100/12))**(self.years*12))/((1 + (self.irate/100/12))**(self.years*12)-1))
+        print("You will need to pay {:.2f} monthly for {} years!".format(monthly, self.years))
+
+def ask_info():
+    sum = int(input("Enter the mortgage amount: "))
+    years = int(input("Enter the number of years: "))
+    irate = float(input("Enter the interest rate: "))
+    return (sum, years, irate)
 
 def main():
-   pass
+    sum, years, irate = ask_info()
+    mortgage = Mortgage(sum, years, irate)
+    mortgage.calculate()
+
+
+if __name__ == "__main__":
+    main()
+
